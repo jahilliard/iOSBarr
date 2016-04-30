19e0e963edb37986b9546cb7661b5c973680d6cf
jahilliard <justin.a.hilliard@gmail.com>
2016-04-29 20:17:00 -0400

    preparing to merge

--- barr/Alamofire.swift
+++ barr/Alamofire.swift
@@ -15,7 +15,7 @@ let headers = [
 
 struct AlamoHelper {
     static let MAX_ATTEMPTS : UInt64 = 5;
-    static let domain = "http://128.237.215.81:3000/"
+    static let domain = "http://192.168.1.49:3001/"
 
     static func authorizedGet(subdomain: String, parameters: [String: AnyObject], completion: (err: NSError?, response: JSON?) -> Void){
         if let accessToken = Me.user.accessToken, userId = Me.user.userId {
